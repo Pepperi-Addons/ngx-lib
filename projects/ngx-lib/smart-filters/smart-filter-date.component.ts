3742b01ea71482f130d3c57cdcfcec4489daf192
tomerp_pepperi <tomer.p@pepperi.com>
2021-01-11 11:50:44 +0200

    - Step for the smart filters.

---
+++ projects/ngx-lib/smart-filters/smart-filter-date.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pep-smart-filter-date',
+    templateUrl: './smart-filter-date.component.html',
+    styleUrls: ['./smart-filter-date.component.scss'],
+})
+export class PepSmartFilterDateComponent implements OnInit {
+    @Input() showTime = false;
+    
+    constructor() {
+        const i = 0;
+    }
+
+    ngOnInit() {
+        const i = 0;
+    }
+}
---
+++ projects/ngx-lib/smart-filters/smart-filter-number.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pep-smart-filter-number',
+    templateUrl: './smart-filter-number.component.html',
+    styleUrls: ['./smart-filter-number.component.scss'],
+})
+export class PepSmartFilterNumberComponent implements OnInit {
+    constructor() {
+        const i = 0;
+    }
+
+    ngOnInit() {
+        const i = 0;
+    }
+}
