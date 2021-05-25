92a518000f306ff34a97a689ac7a28b0fd8d07c1
tomerp_pepperi <tomer.p@pepperi.com>
2021-05-25 22:37:30 +0300

    - Upgrade ngx-lib & storybook (to angular 12 & webpack 5) !!!

--- stories/Header.stories.ts
+++ stories/Header.stories.ts
@@ -7,23 +7,23 @@ import Button from './button.component';
 import Header from './header.component';
 
 export default {
-    title: 'Example/Header',
-    component: Header,
-    decorators: [
-        moduleMetadata({
-            declarations: [Button],
-            imports: [CommonModule],
-        }),
-    ],
+  title: 'Example/Header',
+  component: Header,
+  decorators: [
+    moduleMetadata({
+      declarations: [Button],
+      imports: [CommonModule],
+    }),
+  ],
 } as Meta;
 
 const Template: Story<Header> = (args: Header) => ({
-    props: args,
+  props: args,
 });
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-    user: {},
+  user: {},
 };
 
 export const LoggedOut = Template.bind({});
