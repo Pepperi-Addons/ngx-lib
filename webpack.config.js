6347bf1f5d919f37232bee0f731c55f175394aa3
tomerp_pepperi <tomer.p@pepperi.com>
2021-05-25 12:15:44 +0300

    - Upgrade to angular 12.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path');
+// // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: {
-        plugin: "./dist/out-tsc/projects/web-app/src/main.js"
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "../../webappthirdparty/projects/plugin-themes_ng9/publish/editors/themes.plugin.bundle.js", // "[name].js",
-        libraryTarget: "system"
-    },
-    devtool: "sourcemap"
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.css$/,
-    //             use: [
-    //                 "style-loader",
-    //                 'css-loader'
-    //             ]
-    //         }
-    //     ]
-    // },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //             template: "./index.html"
-    //         }
-    //     )
-    // ]
-};
+// module.exports = {
+//     entry: {
+//         plugin: "./dist/out-tsc/projects/web-app/src/main.js"
+//     },
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: "../../webappthirdparty/projects/plugin-themes_ng9/publish/editors/themes.plugin.bundle.js", // "[name].js",
+//         libraryTarget: "system"
+//     },
+//     devtool: "sourcemap"
+//     // module: {
+//     //     rules: [
+//     //         {
+//     //             test: /\.css$/,
+//     //             use: [
+//     //                 "style-loader",
+//     //                 'css-loader'
+//     //             ]
+//     //         }
+//     //     ]
+//     // },
+//     // plugins: [
+//     //     new HtmlWebpackPlugin({
+//     //             template: "./index.html"
+//     //         }
+//     //     )
+//     // ]
+// };
