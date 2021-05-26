5450c25454035dc9e8068af7981dcefabeec3608
Tomer Popik <66414603+TomerPopik@users.noreply.github.com>
2021-05-26 16:37:29 +0300

    Storybook (#63)
    * - First storybook commit
    
    * no message
    
    * - Added pep-textbox component
    
    * - Move step forword with storybook.
    
    * - Added buttons stories.
    
    * - ts lint
    
    * - Remov knobs and add some buttons stories.
    
    * - Added some stories & properties.
    
    * - Added some stories & properties.
    
    * - Added style state to the button.
    
    * no message
    
    * - Upgrade storybook.
    - Improve some things:
       1. generate all icons.
       2. handle the updating of the playground.
       3. fix some stories.
    
    * Added storybook deployment process
    
    * removed size detector module
    
    * changes storybook deployment folder
    
    * - Added color service.
    - Added icon stories.
    - Added attachment stories.
    - Added checkbox stories.
    
    * - Added theme.
    - Added color stories.
    
    * - Added stories.
    - Refactor some of the settings & stories.
    
    * - Added more stories.
    
    * - Change required to mandatory.
    - Remove the last focused field & control type from the value change event.
    - Change the value change event to the leaf components.
    - Publish new version.
    
    * - Refactiring some code.
    
    * - Bugs fix.
    - Refactoring.
    - Fix lint.
    
    * - Fix lint.
    
    * - Added rich-html-textarea
    - Fix lint.
    
    * - Added sizeType to the search component.
    - Added sizes classes for mat-form-field
    - Increment version number.
    
    * ignore output directories
    
    * Changed Deployment token
    
    * Changed deployer name & email
    
    * - Add search stories & refactoring.
    
    * reverted authentication changes
    
    * no message
    
    * no message
    
    * - Added dialog stories.
    - Refactoring some code.
    - Increment version number.
    
    * - Bug fix. (DI-18142).
    - Publish new version.
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * - Try fix the assets.
    
    * no message
    
    * - Increment version number.
    
    * - Try fix the assets.
    - Increment version number.
    
    * - Try fix the assets.
    - Increment version number.
    
    * no message
    
    * - Bug fix.
    
    * - Increment version number.
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * - Upgrade to angular 12.
    
    * - Upgrade ngx-lib & storybook (to angular 12 & webpack 5) !!!
    
    * - Added chrimatic CI
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    Co-authored-by: roiaharon-pepperi <66308169+roiaharon-pepperi@users.noreply.github.com>

--- stories/button.component.ts
+++ stories/button.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     [ngClass]="classes"
     [ngStyle]="{ 'background-color': backgroundColor }"
   >
-    {{ label }}
+    {{ label }} test
   </button>`,
   styleUrls: ['./button.css'],
 })
