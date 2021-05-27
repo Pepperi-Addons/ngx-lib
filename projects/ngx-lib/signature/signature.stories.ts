7376ed03f49b893214a62e9dceeab125f4fef5f5
Tomer Popik <66414603+TomerPopik@users.noreply.github.com>
2021-05-27 16:12:56 +0300

    Storybook (#66)
    * - First storybook commit
    
    * no message
    
    * - Added pep-textbox component
    
    * - Move step forword with storybook.
    
    * - Added buttons stories.
    
    * - ts lint
    
    * - Remov knobs and add some buttons stories.
    
    * - Added some stories & properties.
    
    * - Added some stories & properties.
    
    * - Added style state to the button.
    
    * no message
    
    * - Upgrade storybook.
    - Improve some things:
       1. generate all icons.
       2. handle the updating of the playground.
       3. fix some stories.
    
    * Added storybook deployment process
    
    * removed size detector module
    
    * changes storybook deployment folder
    
    * - Added color service.
    - Added icon stories.
    - Added attachment stories.
    - Added checkbox stories.
    
    * - Added theme.
    - Added color stories.
    
    * - Added stories.
    - Refactor some of the settings & stories.
    
    * - Added more stories.
    
    * - Change required to mandatory.
    - Remove the last focused field & control type from the value change event.
    - Change the value change event to the leaf components.
    - Publish new version.
    
    * - Refactiring some code.
    
    * - Bugs fix.
    - Refactoring.
    - Fix lint.
    
    * - Fix lint.
    
    * - Added rich-html-textarea
    - Fix lint.
    
    * - Added sizeType to the search component.
    - Added sizes classes for mat-form-field
    - Increment version number.
    
    * ignore output directories
    
    * Changed Deployment token
    
    * Changed deployer name & email
    
    * - Add search stories & refactoring.
    
    * reverted authentication changes
    
    * no message
    
    * no message
    
    * - Added dialog stories.
    - Refactoring some code.
    - Increment version number.
    
    * - Bug fix. (DI-18142).
    - Publish new version.
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * - Try fix the assets.
    
    * no message
    
    * - Increment version number.
    
    * - Try fix the assets.
    - Increment version number.
    
    * - Try fix the assets.
    - Increment version number.
    
    * no message
    
    * - Bug fix.
    
    * - Increment version number.
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * - Upgrade to angular 12.
    
    * - Upgrade ngx-lib & storybook (to angular 12 & webpack 5) !!!
    
    * - Added chrimatic CI
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * - Added lint
    
    * - Change web-app to ngx-lib-tester
    
    * no message
    
    * - Update package.json files.
    - Fix signature.
    - Add signature into storybook.
    - Increment lib version.
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    Co-authored-by: roiaharon-pepperi <66308169+roiaharon-pepperi@users.noreply.github.com>

---
+++ projects/ngx-lib/signature/signature.stories.ts
@@ -0,0 +1,70 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { SBNgxHelperModule } from '@storybook-settings/ngx-helper.module';
+import { commonArgTypes } from '@storybook-settings/common-args.model';
+
+import { PepSignatureComponent } from '../signature/signature.component';
+import { PepSignatureModule } from '../signature/signature.module';
+
+// This exports the Stories group for this component
+export default {
+    title: 'Components/signature',
+    component: PepSignatureComponent,
+    // argTypes: argTypesBasicStory,
+    decorators: [
+        // The necessary modules for the component to work on Storybook
+        moduleMetadata({
+            imports: [PepSignatureModule, SBNgxHelperModule],
+        }),
+    ],
+    args: {
+        rowSpan: 4,
+    },
+    argTypes: {
+        label: commonArgTypes.label,
+        mandatory: commonArgTypes.mandatory,
+        xAlignment: commonArgTypes.xAlignment,
+        disabled: commonArgTypes.disabled,
+        showTitle: commonArgTypes.showTitle,
+        src: {
+            description: 'This is the src of the signature',
+            table: {
+                defaultValue: { summary: null },
+            },
+        },
+        rowSpan: commonArgTypes.rowSpan,
+        fileChange: commonArgTypes.fileChange,
+    },
+    parameters: {
+        controls: {
+            include: [
+                'label',
+                'src',
+                'mandatory',
+                'disabled',
+                'xAlignment',
+                'rowSpan', // 'srcLarge',
+                'showTitle',
+                'elementClick',
+                'fileChange',
+            ],
+        },
+    },
+} as Meta;
+
+// This creates a Story for the component
+const Template: Story<PepSignatureComponent> = (args: PepSignatureComponent) => ({
+    props: {
+        ...args,
+    },
+    // [srcLarge]="srcLarge"
+    template: `
+        <pep-signature [label]="label" [src]="src" [mandatory]="mandatory" [disabled]="disabled" [xAlignment]="xAlignment" [rowSpan]="rowSpan"
+        [showTitle]="showTitle" (fileChange)="fileChange($event)"></pep-signature>
+    `,
+});
+
+export const Base = Template.bind({});
+Base.storyName = 'Basic';
+Base.args = {
+    src: 'https://i.ibb.co/VMHwLkm/58957776-8700-4c6a-b9bc-a171b84d8080.png'
+};
