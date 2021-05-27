3a3ed3545fe2a4954539a9b5c0c61e3de4ae61b2
tomerp_pepperi <tomer.p@pepperi.com>
2021-05-27 12:50:23 +0300

    - Change web-app to ngx-lib-tester

--- projects/ngx-lib-tester/karma.conf.js
+++ projects/ngx-lib-tester/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, '../../coverage/web-app'),
+            dir: require('path').join(__dirname, '../../coverage/ngx-lib-tester'),
             reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
         },
