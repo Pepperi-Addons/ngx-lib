8d69620bee596a399a715ed3571458bd4d83fe94
tomerp_pepperi <tomer.p@pepperi.com>
2022-01-12 12:28:14 +0200

    - Publish version with the scss changes.

--- projects/ngx-lib-tester/src/app/app.component.ts
+++ projects/ngx-lib-tester/src/app/app.component.ts
@@ -9,7 +9,7 @@ import {
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+    styleUrls: ['./app.component.scss', './app.component.theme.scss'],
 })
 export class AppComponent implements OnInit {
     footerHeight: number;
