4a334a3820adfac8a59a899eb6d3e653b569cf21
Yaron Shafir <yaron.s@pepperi.com>
2022-05-11 10:39:16 +0300

    regex link bug fix

--- projects/ngx-lib/link/link.pipes.ts
+++ projects/ngx-lib/link/link.pipes.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IsUrlPipe implements PipeTransform {
     transform(value: string): boolean {
-        const regex = /^((http(s?)|ftp)\:\/\/)(www\.)|(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)(.*)/;
-       
+        //const regex = /^(((http(s?)|ftp)\:\/\/)|(www\.))[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)(.*)/;
+        const regex = /^(((http(s?)|ftp)\:\/\/)|(www\.))/;
         return regex.test(value);
     }
 }
