5ad224f84944c051d3d7b1dd43525be186f9038e
Avner Eisenberg <avner.e@pepperi.com>
2022-11-22 14:15:24 +0200

    no message

--- projects/ngx-lib/select-panel/select-panel.stories.ts
+++ projects/ngx-lib/select-panel/select-panel.stories.ts
@@ -1,4 +1,7 @@
 import { moduleMetadata, Story, Meta } from '@storybook/angular';
+
+// import { withKnobs, text, select, radios } from '@storybook/addon-knobs';
+
 import { SBNgxHelperModule } from '@storybook-settings/ngx-helper.module';
 import { commonArgTypes } from '@storybook-settings/common-args.model';
 
@@ -9,135 +12,98 @@ import { action } from '@storybook/addon-actions';
 
 // This exports the Stories group for this component
 export default {
-    title: 'Components/button',
+    title: 'Components/select',
     component: PepSelectPanelComponent,
-    // argTypes: argTypesBasicStory,
-    decorators: [
-        // The necessary modules for the component to work on Storybook
-        moduleMetadata({
-            imports: [PepSelectPanelModule, SBNgxHelperModule],
-        }),
-    ],
-    // args: {
-    // },
     argTypes: {
+        label: commonArgTypes.label,
         value: commonArgTypes.value,
-        visible: commonArgTypes.visible,
-        disabled: commonArgTypes.disabled,
-        iconPosition: {
-            table: {
-                defaultValue: { summary: 'end' },
+        xAlignment: commonArgTypes.xAlignment,
+        mandatory: commonArgTypes.mandatory,
+        showTitle: commonArgTypes.showTitle,
+        type: {
+            description: 'This is the type of the component',
+            defaultValue: 'select',
+            control: {
+                type: 'radio',
+                options: ['select', 'multi'],
             },
         },
-        iconName: commonArgTypes.iconName,
-        sizeType: commonArgTypes.sizeType,
-        styleStateType: commonArgTypes.styleStateType,
-        styleType: commonArgTypes.styleType,
-        classNames: commonArgTypes.classNames,
-        buttonClick: {
-            action: 'buttonClick',
-            control: false,
-        },
+        disabled: commonArgTypes.disabled,
+        valueChange: commonArgTypes.valueChange,
     },
     parameters: {
         controls: {
             include: [
+                'label',
                 'value',
-                'visible',
+                'xAlignment',
+                'type',
+                'mandatory',
                 'disabled',
-                'iconPosition',
-                'iconName',
-                'sizeType',
-                'styleStateType',
-                'styleType',
-                'classNames',
-                'buttonClick',
+                'showTitle',
+                'options',
+                'valueChange',
             ],
         },
     },
+    decorators: [
+        moduleMetadata({
+            imports: [PepSelectPanelModule, SBNgxHelperModule],
+        }),
+    ],
 } as Meta;
 
-// This creates a Story for the component
 const Template: Story<PepSelectPanelComponent> = (args: PepSelectPanelComponent) => ({
+    // component: PepSelectComponent,
     props: {
         ...args,
-        buttonClick: action('buttonClick'),
+        valueChange: action('valueChange'),
     },
     // template: `
-    //     <pep-button [value]="value" [disabled]="disabled" [styleType]="styleType" [styleStateType]="styleStateType" [sizeType]="sizeType" [iconName]="iconName"
-    //     [iconPosition]="iconPosition" [visible]="visible" (buttonClick)="buttonClick($event)"></pep-button>
+    //     <pep-select [value]="value" [label]="label" [type]="type" [mandatory]="mandatory" [disabled]="disabled"
+    //     [xAlignment]="xAlignment" [options]="options" [showTitle]="showTitle"
+    //     (valueChange)="valueChange($event)"></pep-select>
     // `,
 });
 
-export const Base = Template.bind({});
-Base.storyName = 'Basic';
-Base.args = {
-    value: 'click me'
+// Basic story
+export const Basic = Template.bind({});
+Basic.args = {
+    label: 'select an option',
+    value: 'test1',
+    options: [
+        { key: 'test1', value: 'test1' },
+        { key: 'test2', value: 'test2' },
+    ],
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-    disabled: true,
-    value: 'disabled',
-};
+// // Other stories could be added here as well, all you have to do is export them along!
+// export const Mandatory: Story<PepSelectComponent> = () => ({
+//     component: PepSelectComponent,
+//     props: {
+//         label: 'mandatory',
+//         value: text('value', 'test1'),
+//         options: [
+//             { key: 'test1', value: 'test1' },
+//             { key: 'test2', value: 'test2' },
+//         ],
+//     },
+//     template: `
+//         <pep-select [label]="label" [value]="value" [options]="options" mandatory="true"></pep-select>
+//     `,
+// });
 
-// This creates a Story for the component
-const StyleTypeTemplate: Story<PepSelectPanelComponent> = (args: PepSelectPanelComponent) => ({
-    props: args,
-    template: `
-    <div style="display: flex; flex-direction: column;">
-        <h3 class="pep-spacing-element" style="margin-bottom:0.5rem;">XL</h3>
-        <div class="flex pep-spacing-element-negative">
-            <pep-button class="pep-spacing-element" value="Weak" styleType="weak" sizeType="xl"></pep-button>
-            <pep-button class="pep-spacing-element" value="Regular" styleType="regular" sizeType="xl">
-            </pep-button>
-            <pep-button class="pep-spacing-element" value="Strong" styleType="strong" sizeType="xl">
-            </pep-button>
-        </div>
-        <h3 class="pep-spacing-element" style="margin-bottom:0.5rem;">LG</h3>
-        <div class="flex pep-spacing-element-negative">
-            <pep-button class="pep-spacing-element" value="Weak" styleType="weak" sizeType="lg"></pep-button>
-            <pep-button class="pep-spacing-element" value="Regular" styleType="regular" sizeType="lg">
-            </pep-button>
-            <pep-button class="pep-spacing-element" value="Strong" styleType="strong" sizeType="lg">
-            </pep-button>
-        </div>
-        <h3 class="pep-spacing-element" style="margin-bottom:0.5rem;">MD</h3>
-        <div class="flex pep-spacing-element-negative">
-            <pep-button class="pep-spacing-element" value="Weak" styleType="weak" sizeType="md"></pep-button>
-            <pep-button class="pep-spacing-element" value="Regular" styleType="regular" sizeType="md">
-            </pep-button>
-            <pep-button class="pep-spacing-element" value="Strong" styleType="strong" sizeType="md">
-            </pep-button>
-        </div>
-        <h3 class="pep-spacing-element" style="margin-bottom:0.5rem;">SM</h3>
-        <div class="flex pep-spacing-element-negative">
-            <pep-button class="pep-spacing-element" value="Weak" styleType="weak" sizeType="sm"></pep-button>
-            <pep-button class="pep-spacing-element" value="Regular" styleType="regular" sizeType="sm">
-            </pep-button>
-            <pep-button class="pep-spacing-element" value="Strong" styleType="strong" sizeType="sm">
-            </pep-button>
-        </div>
-        <h3 class="pep-spacing-element" style="margin-bottom:0.5rem;">XS</h3>
-        <div class="flex pep-spacing-element-negative">
-            <pep-button class="pep-spacing-element" value="Weak" styleType="weak" sizeType="xs"></pep-button>
-            <pep-button class="pep-spacing-element" value="Regular" styleType="regular" sizeType="xs">
-            </pep-button>
-            <pep-button class="pep-spacing-element" value="Strong" styleType="strong" sizeType="xs">
-            </pep-button>
-        </div>
-    </div>
-`,
-});
-
-export const StyleType = StyleTypeTemplate.bind({});
-// StyleType.args = {
-//     styleType: 'strong',
-//     value: 'strong',
-// };
-// StyleType.argTypes = argTypesOtherStories;
-StyleType.storyName = '3 style types';
-StyleType.parameters = {
-    // docs: { iframeHeight: 500 },
-    // controls: { hideNoControlsWarning: true },
-};
+// export const Multi: Story<PepSelectComponent> = () => ({
+//     component: PepSelectComponent,
+//     props: {
+//         label: 'multi',
+//         value: text('value', 'test1'),
+//         options: [
+//             { key: 'test1', value: 'test1' },
+//             { key: 'test2', value: 'test2' },
+//         ],
+//     },
+//     template: `
+//         <pep-select [label]="label" type="multi" [value]="value" [options]="options"></pep-select>
+//     `,
+// });
