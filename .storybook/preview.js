0aab392b4981969540c4f060890233ebaa8ed8fd
Yonatan Kof <kof@yonatankof.com>
2023-03-06 12:09:20 +0200

    Add and override Storybook styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,11 +2,25 @@
 
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
+import { styled } from '@storybook/theming';
+
 setCompodocJson(docJson);
 
 // Added theme style
 import '!style-loader!css-loader!sass-loader!./theme.scss';
 import '!style-loader!css-loader!sass-loader!./storybook-styles.scss';
+
+
+const divEmpty = styled.div(({ theme }) => ({
+}));
+const pEmpty = styled.p(({ theme }) => ({
+//example of custom font
+fontFamily: "Inter"
+}));
+const spanEmpty = styled.span(({ theme }) => ({
+}));
+const inputEmpty = styled.input(({ theme }) => ({
+}));
 
 export const parameters = {
     layout: 'centered',
@@ -20,7 +34,13 @@ export const parameters = {
     viewMode: 'docs',
     // previewTabs: { 'storybook/docs/panel': { index: -1 } },
     docs: {
-        inlineStories: true
+        inlineStories: true,
+        components: {
+            p: pEmpty,
+            div: divEmpty,
+            span: spanEmpty,
+            input: inputEmpty
+        },
     },
 }
 
