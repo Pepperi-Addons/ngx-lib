749e49916a702a43e770384be0a3d66d73046787
Yonatan Kof <kof@yonatankof.com>
2023-03-22 14:50:40 +0200

    Add Rich HTML textarea story

--- projects/ngx-lib/rich-html-textarea/rich-html-textarea.stories.ts
+++ projects/ngx-lib/rich-html-textarea/rich-html-textarea.stories.ts
@@ -10,12 +10,9 @@ export default {
     // The title defines the name and where in the structure of
     // Storybook's menu this is going to be placed.
     // Here we add it to a "Components" section under "rich-html-textarea"
-    title: 'Components/rich-html-textarea',
+    title: 'Components/Rich HTML textarea',
     // The component related to the Stories
     component: PepRichHtmlTextareaComponent,
-    args: {
-        rowSpan: 8,
-    },
     argTypes: {
         label: commonArgTypes.label,
         value: commonArgTypes.value, // { control: { type: 'number', min: 0, max: 10 } },
@@ -76,12 +73,54 @@ const Template: Story<PepRichHtmlTextareaComponent> = (
     // `,
 });
 
-export const Base = Template.bind({});
-Base.args = {
-    label: 'rich-html-textarea',
-    value: `<div class='body-sm rich-text-cont ng-star-inserted' style='height: 27.5rem; text-align: left;'><h1 class='title-4xl'>B07MZ3</h1><p class='body-md'>Brand: <span style='color: #ff0000;'>
-    <strong>SojoS</strong></span></p><p class='body-md'>Product Code: <span style='color: #ff0000;'><strong>She Young</strong></span></p><p class='body-md'>List Price: <span style='color: #ff0000;'>
-    <strong>12.9</strong></span></p><h2 class='title-2xl'>Product Details</h2><p></p><ul style='list-style: none;'><li style='background: url(https://static.zennioptical.com/dev/image/site/frame-size-icons/frame-width.svg) no-repeat; line-height: 2.5rem;'>
-    <p style='padding-left: 50px;'>Frame Width: 132mm</p></li><li style='background: url(https://static.zennioptical.com/dev/image/site/frame-size-icons/lens-width.svg) no-repeat; line-height: 2.5rem;'><p style='padding-left: 50px;'>Lens Width: 52mm</p></li>
-    <li style='background: url(https://static.zennioptical.com/dev/image/site/frame-size-icons/bridge.svg) no-repeat; line-height: 2.5rem;'><p style='padding-left: 50px;'>Bridge: 16mm</p></li></ul><p></p></div>`,
+const thisValue = `<style>main{font-family:sans-serif}strong{color:#b68300}ul{list-style:none;padding:0 .5rem;margin:0}li{display:flex;flex-direction:row;align-items:center;gap:1rem}img{height:auto;width:2rem}p{font-size:1rem;margin-block:1rem}h2{margin:0}</style><main><h1>B07MZ3</h1><p>Brand: <strong>SojoS</strong></p><p>Product Code: <strong>She Young</strong></p><p>List Price: <strong>12.9</strong></p><h2>Product Details</h2><ul><li><img src="https://static.zennioptical.com/dev/image/site/frame-size-icons/frame-width.svg"><p>Frame Width: <strong>132mm</strong></p></li><li><img src="https://static.zennioptical.com/dev/image/site/frame-size-icons/lens-width.svg"><p>Lens Width: <strong>52mm</strong></p></li><li><img src="https://static.zennioptical.com/dev/image/site/frame-size-icons/bridge.svg"><p>Bridge: <strong>16mm</strong></p></li></ul></main>`;
+
+export const Story1 = Template.bind({});
+Story1.storyName = 'Basic';
+Story1.args = {
+    label: 'Rich HTML textarea',
+    rowSpan: 6,
+    value: thisValue,
+    disabled: true,
+};
+
+export const Story2 = Template.bind({});
+Story2.storyName = 'Read only';
+Story2.args = {
+    label: 'Read only rich HTML textarea',
+    rowSpan: 6,
+    value: thisValue,
+    disabled: true,
+};
+
+export const Story3 = Template.bind({});
+Story3.storyName = 'Editable';
+Story3.args = {
+    label: 'Editable rich HTML textarea',
+    rowSpan: 6,
+    value: thisValue,
+};
+
+export const Story4 = Template.bind({});
+Story4.storyName = 'Empty';
+Story4.args = {
+    label: 'Empty rich HTML textarea',
+    rowSpan: 4,
+};
+
+export const Story5 = Template.bind({});
+Story5.storyName = 'Inline, no content';
+Story5.args = {
+    label: 'Inline, no content rich HTML textarea',
+    rowSpan: 4,
+    inlineMode: true,
+};
+
+export const Story6 = Template.bind({});
+Story6.storyName = 'Inline, with content';
+Story6.args = {
+    label: 'Inline, with content rich HTML textarea',
+    value: thisValue,
+    rowSpan: 4,
+    inlineMode: true,
 };
